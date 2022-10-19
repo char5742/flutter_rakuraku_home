64133efa5e8de05af0e5e9b89ac0c7f197164727
char5742 <f.shuuma@gmail.com>
2022-10-20 00:44:58 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_rakuraku_home/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.flutter_rakuraku_home
+
+import android.content.pm.PackageManager
+import androidx.annotation.NonNull
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+
+
+class MainActivity: FlutterActivity() {
+    
+}
